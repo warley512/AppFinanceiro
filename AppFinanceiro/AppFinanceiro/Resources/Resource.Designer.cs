da5c4c5bc925352c12d19d9b6a3b24b20e2be32a
joao1315 <joaogabrieloliveira9@gmail.com>
2016-06-07 15:23:03 -0300

    Incluido SQLite, mudanca do tema do layout Main.

--- AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
+++ AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
@@ -57,22 +57,6 @@ namespace AppFinanceiro
 			}
 		}
 		
-		public partial class Id
-		{
-			
-			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
-			
-			static Id()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Id()
-			{
-			}
-		}
-		
 		public partial class Layout
 		{
 			
