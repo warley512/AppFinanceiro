ed1d357d07f9656fb92b723f371fa9e8b6b4e182
VictorPrado <victorprado30@gmail.com>
2016-06-08 14:21:15 -0300

    Inserir credito

---
+++ AppFinanceiro/AppFinanceiro/Resources/InserirCredito.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace AppFinanceiro.Resources {
+    class InserirCredito {
+        double valor = 0;
+        string descricao = "";
+
+        public InserirCredito(double valor, string descricao) {
+            this.valor = valor;
+            this.descricao = descricao;
+        }
+
+
+
+        }
+    }
