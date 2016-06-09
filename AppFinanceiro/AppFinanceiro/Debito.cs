983aa20824b9a567c6224003c56d8e2f464525cf
Warley512 <warley.peres@hotmail.com>
2016-06-09 16:53:04 -0300

    Adição da funcionalidade Time Picker

--- AppFinanceiro/AppFinanceiro/Debito.cs
+++ AppFinanceiro/AppFinanceiro/Debito.cs
@@ -21,6 +21,17 @@ namespace AppFinanceiro
         public double valor { get; set; }
         public string descricao { get; set; }
         public string tipoConta { get; set; }
+        TextView _dateDisplay;
+        Button _dateSelectButton;
+
+        private TextView time_display;
+        private Button pick_button;
+
+        private int hour;
+        private int minute;
+
+        const int TIME_DIALOG_ID = 0;
+
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -73,11 +84,48 @@ namespace AppFinanceiro
             spinnerCategoria.Adapter = adapterCategorias;
 
             //implementação hora e data
-            //_dateDisplay = FindViewById<TextView>(Resource.Id.date_display);
-            //_dateSelectButton = FindViewById<Button>(Resource.Id.date_select_button);
-            //_dateSelectButton.Click += DateSelect_OnClick;
+           // _dateDisplay = FindViewById<TextView>(Resource.Id.date_display);
+            _dateSelectButton = FindViewById<Button>(Resource.Id.date_select_button);
+            _dateSelectButton.Click += DateSelect_OnClick;
+
+            //implementação hora
+            // Capture our View elements
+            time_display = FindViewById<TextView>(Resource.Id.timeDisplay);
+            pick_button = FindViewById<Button>(Resource.Id.pickTime);
+
+            // Add a click listener to the button
+            pick_button.Click += (o, e) => ShowDialog(TIME_DIALOG_ID);
+
+            // Get the current time
+            hour = DateTime.Now.Hour;
+            minute = DateTime.Now.Minute;
+
+            // Display the current date
+            UpdateDisplay();
 
 
+
+        }
+
+        protected override Dialog OnCreateDialog(int id)
+        {
+            if (id == TIME_DIALOG_ID)
+                return new TimePickerDialog(this, TimePickerCallback, hour, minute, false);
+
+            return null;
+        }
+
+        private void TimePickerCallback(object sender, TimePickerDialog.TimeSetEventArgs e)
+        {
+            hour = e.HourOfDay;
+            minute = e.Minute;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            string time = string.Format("{0}:{1}", hour, minute.ToString().PadLeft(2, '0'));
+            time_display.Text = time;
         }
 
         void DateSelect_OnClick(object sender, EventArgs eventArgs)
--- AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
+++ AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
@@ -85,20 +85,20 @@ namespace AppFinanceiro
 			// aapt resource value: 0x7f060002
 			public const int button1 = 2131099650;
 			
+			// aapt resource value: 0x7f060009
+			public const int date_select_button = 2131099657;
+			
 			// aapt resource value: 0x7f060006
 			public const int editText1 = 2131099654;
-			
-			// aapt resource value: 0x7f060009
-			public const int editText2 = 2131099657;
-			
-			// aapt resource value: 0x7f06000a
-			public const int editText3 = 2131099658;
 			
 			// aapt resource value: 0x7f060003
 			public const int imagemItem = 2131099651;
 			
+			// aapt resource value: 0x7f06000c
+			public const int listaDados = 2131099660;
+			
 			// aapt resource value: 0x7f06000b
-			public const int listaDados = 2131099659;
+			public const int pickTime = 2131099659;
 			
 			// aapt resource value: 0x7f060001
 			public const int space1 = 2131099649;
@@ -114,6 +114,9 @@ namespace AppFinanceiro
 			
 			// aapt resource value: 0x7f060004
 			public const int textLabel = 2131099652;
+			
+			// aapt resource value: 0x7f06000a
+			public const int timeDisplay = 2131099658;
 			
 			// aapt resource value: 0x7f060005
 			public const int valorDebito = 2131099653;
