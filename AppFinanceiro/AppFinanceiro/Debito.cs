af2869f5dbca81c111525d88b591164dd58081f5
VictorPrado <victorprado30@gmail.com>
2016-06-09 14:57:54 -0300

    error

--- AppFinanceiro/AppFinanceiro/Debito.cs
+++ AppFinanceiro/AppFinanceiro/Debito.cs
@@ -9,10 +9,13 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
+using System.IO;
+using AppFinanceiro.BD;
 
 namespace AppFinanceiro
 {
-    [Activity(Label = "AppFinanceiro")]
+    [Activity(Label = "Lançamento")]
     public class Debito : Activity
     {
         public double valor { get; set; }
@@ -25,6 +28,40 @@ namespace AppFinanceiro
             SetContentView(Resource.Layout.Debito);
 
             FindViewById<EditText>(Resource.Id.valorDebito).Click += valorDebitoFocus;
+
+            var db = new SQLiteConnection(Path.Combine(
+               System.Environment.GetFolderPath(
+                   System.Environment.SpecialFolder.MyDocuments), "DB_Financeiro"
+               )
+           );
+
+            //List<BD_Usuario> DadosUser = db.Table<BD_Usuario>().ToList();
+            //List<String> listCategorias = new List<string>();
+
+            //for (int i = 0; i < DadosUser.Count; i++)
+            //{
+            //    listCategorias.Add(DadosUser[i].nome);
+            //}
+
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner1);
+
+           spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(
+                     this, Resource.Array.account_types, Android.Resource.Layout.SimpleSpinnerItem);
+            //var adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem, listCategorias);
+
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+
+
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+
+            string toast = string.Format("The planet is {0}", spinner.GetItemAtPosition(e.Position));
+            Toast.MakeText(this, toast, ToastLength.Long).Show();
         }
 
         public void valorDebitoFocus(object sender, EventArgs e)
@@ -41,6 +78,8 @@ namespace AppFinanceiro
 
         }
         */
+
+
 
 
 
