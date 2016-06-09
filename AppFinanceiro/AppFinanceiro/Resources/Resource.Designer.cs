b6fe7cd2c85e2e64d8c064e558a94a113a973c46
joao1315 <joaogabrieloliveira9@gmail.com>
2016-06-09 16:35:53 -0300

    Correção bug nos Resource.Id

--- AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
+++ AppFinanceiro/AppFinanceiro/Resources/Resource.Designer.cs
@@ -94,69 +94,68 @@ namespace AppFinanceiro
 			// aapt resource value: 0x7f060002
 			public const int button1 = 2131099650;
 			
-			// aapt resource value: 0x7f060006
-			public const int editText1 = 2131099654;
-			
 			// aapt resource value: 0x7f060009
-			public const int editText2 = 2131099657;
+			public const int categoriaOperacao = 2131099657;
 			
-			// aapt resource value: 0x7f06000a
-			public const int editText3 = 2131099658;
+			// aapt resource value: 0x7f060007
+			public const int dataOperacao = 2131099655;
+			
+			// aapt resource value: 0x7f06000b
+			public const int editText1 = 2131099659;
+			
+			// aapt resource value: 0x7f06000e
+			public const int editText2 = 2131099662;
+			
+			// aapt resource value: 0x7f06000f
+			public const int editText3 = 2131099663;
+			
+			// aapt resource value: 0x7f060005
+			public const int iconOperacao = 2131099653;
 			
 			// aapt resource value: 0x7f060003
 			public const int imagemItem = 2131099651;
 			
-			// aapt resource value: 0x7f06000b
-			public const int listaDados = 2131099659;
+			// aapt resource value: 0x7f060010
+			public const int listaDados = 2131099664;
 			
-			// aapt resource value: 0x7f050009
-			public const int editText1 = 2131034121;
+			// aapt resource value: 0x7f060015
+			public const int lista_lancamentos = 2131099669;
 			
-			// aapt resource value: 0x7f050002
-			public const int iconOperacao = 2131034114;
+			// aapt resource value: 0x7f060006
+			public const int nomeOperacao = 2131099654;
 			
-			// aapt resource value: 0x7f050000
-			public const int imagemItem = 2131034112;
+			// aapt resource value: 0x7f060011
+			public const int resumoGastos = 2131099665;
 			
-			// aapt resource value: 0x7f05000a
-			public const int listaDados = 2131034122;
+			// aapt resource value: 0x7f060001
+			public const int space1 = 2131099649;
 			
-			// aapt resource value: 0x7f05000f
-			public const int lista_lancamentos = 2131034127;
-			
-			// aapt resource value: 0x7f050003
-			public const int nomeOperacao = 2131034115;
-			
-			// aapt resource value: 0x7f05000b
-			public const int resumoGastos = 2131034123;
 			// aapt resource value: 0x7f060000
 			public const int spinnerAdd = 2131099648;
 			
+			// aapt resource value: 0x7f06000d
+			public const int spinnerCategoria = 2131099661;
+			
+			// aapt resource value: 0x7f06000c
+			public const int spinnerDebitoTipoLanc = 2131099660;
+			
+			// aapt resource value: 0x7f060004
+			public const int textLabel = 2131099652;
+			
+			// aapt resource value: 0x7f060012
+			public const int totalCreditos = 2131099666;
+			
+			// aapt resource value: 0x7f060013
+			public const int totalDebitos = 2131099667;
+			
+			// aapt resource value: 0x7f060014
+			public const int totalFinal = 2131099668;
+			
+			// aapt resource value: 0x7f06000a
+			public const int valorDebito = 2131099658;
+			
 			// aapt resource value: 0x7f060008
-			public const int spinnerCategoria = 2131099656;
-			
-			// aapt resource value: 0x7f060007
-			public const int spinnerDebitoTipoLanc = 2131099655;
-			
-			// aapt resource value: 0x7f050007
-			public const int textViewInserirDebito = 2131034119;
-			
-			// aapt resource value: 0x7f05000c
-			public const int totalCreditos = 2131034124;
-			
-			// aapt resource value: 0x7f05000d
-			public const int totalDebitos = 2131034125;
-			
-			// aapt resource value: 0x7f05000e
-			public const int totalFinal = 2131034126;
-			
-			// aapt resource value: 0x7f050008
-			public const int valorDebito = 2131034120;
-			
-			// aapt resource value: 0x7f050005
-			public const int valorOperacao = 2131034117;
-			// aapt resource value: 0x7f060005
-			public const int valorDebito = 2131099653;
+			public const int valorOperacao = 2131099656;
 			
 			static Id()
 			{
